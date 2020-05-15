8554f70338fb6e397b1ee470ec5fce521f1b649f
edwardgc <edwardgc@gmail.com>
2020-05-15 09:20:58 +0800

    format code

--- mfc-project/src/MainApp.cpp
+++ mfc-project/src/MainApp.cpp
@@ -4,76 +4,44 @@ MyApp app;
 
 BOOL MyApp::InitInstance()
 {
-
-    //创建窗口
-
     MyFrame *frame = new MyFrame;
-
-    //显示和更新
-
     frame->ShowWindow(SW_SHOWNORMAL);
-
     frame->UpdateWindow();
-
     m_pMainWnd = frame; //保存指向应用程序的主窗口的指针
-
-    return TRUE; //返回正常初始化
+    return TRUE;
 }
 
-//分界宏
-
+//注册消息处理函数
 BEGIN_MESSAGE_MAP(MyFrame, CFrameWnd)
 
-ON_WM_LBUTTONDOWN() //鼠标左键按下
-
+ON_WM_LBUTTONDOWN()
 ON_WM_CHAR()
-
 ON_WM_PAINT()
 
 END_MESSAGE_MAP()
 
 MyFrame::MyFrame()
-
 {
-
     Create(NULL, TEXT("windows"));
 }
 
 void MyFrame::OnLButtonDown(UINT, CPoint point)
-
 {
-
-    //TCHAR buf[1024];
-
-    //wsprintf(buf, TEXT("x = %d,y = %d"), point.x, point.y);
-
-    //MessageBox(buf);
-
     CString str;
-
     str.Format(TEXT("x = % d, y = % d"), point.x, point.y);
-
     MessageBox(str);
 }
 
 void MyFrame::OnChar(UINT key, UINT, UINT)
-
 {
-
     CString str;
-
     str.Format(TEXT("输入 %c"), key);
-
     MessageBox(str);
 }
 
 void MyFrame::OnPaint()
-
 {
-
     CPaintDC dc(this);
-
     dc.TextOut(100, 100, TEXT("Hello World"));
-
     dc.Ellipse(10, 10, 100, 100);
 }
